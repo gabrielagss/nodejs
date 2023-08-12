59a6ffadb91f9dd3ef0b15b13c6f4f3b527b4091
gabriela <116124586+gabrielagss@users.noreply.github.com>
2023-08-11 22:15:45 -0300

    Add files via upload

--- fundamentos/media.js
+++ fundamentos/media.js
@@ -1,13 +1,18 @@
 /**
  * App para calcular a média de duas notas 
  */
+//importação do pacote readline-sync
 
-let nome, disciplina // cria uma variavel
-let nota1, nota2, nota3, media // criar variaveis 
+const read = require('readline-sync') 
+//variaveis 
+let nome, disciplina 
+let nota1, nota2, media // let criar variaveis 
 
-console.clear// entragar o resultado / a parte que vai exibir o texto na tela 
-console.clear 
+// entragar o resultado 
 
+
+// a parte que vai exibir o texto na tela
+console.clear ( )
 console.log("____            ___           __                       ")
 console.log("/\\  _`\\         /\\_ \\         /\\ \\__  __                ")
 console.log("\\ \\ \L\\ \\    ___\\//\\ \\      __\\ \\ ,_\\/\\_\\    ___ ___    ")
@@ -16,17 +21,21 @@ console.log("  \\ \\ \\L\\ \\/\\ \\L\\ \\\\_\\ \\_/\\  __/\\ \\ \\_\\ \\ \\/\\ \
 console.log("   \\ \\____/\\ \\____//\\____\\ \\____\\\\ \\__\\\\ \\_\\ \\_\\ \\_\\ \\_\\ ")
 console.log("    \\/___/  \\/___/ \\/____/\\/____/ \/\__/ \\/_/\\/_/\\/_/\\/\_/ ")
 
-nome = "Gabriela Gomes da Silva"
-disciplina = "Matemática"
-nota1 = 5 // dar o valor da variavel 
-nota2 = 7.5
-nota3 = 3.7
+//entrada
+nome = read.question('Digite um nome:')
+disciplina = read.question('Digite uma disciplina:')
+nota1 = Number(read.question('Digite a Nota1:'))  
+nota2 = Number(read.question('Digite a Nota2:'))
 
-// saida
-media = (nota1 + nota2 + nota3) / 2
 
-console.log(`Nome: ${nome}`)
+// processamento 
+media = (nota1 + nota2 ) / 2
+//saida
+console.clear()
+console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ ")
+console.log(`Nome: ${nome}`) // console.log = atribuir o valor variavel
 console.log(`Disciplina: ${disciplina}`)
 console.log(`Nota1: ${nota1}`)
 console.log(`Nota2: ${nota2}`)
 console.log(`Média final: ${media}`)
+console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ ")
